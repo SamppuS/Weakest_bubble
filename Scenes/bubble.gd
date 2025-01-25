af1b807d990c39e0e0b60862a905bb2f5a7b7506
Ildeuz1 <148572986+Ildeuz1@users.noreply.github.com>
2025-01-25 12:53:39 +0200

    JK

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -9,7 +9,7 @@ const radius = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Camera2D.make_current()
+	%Camera2D.make_current()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
