7143280c0fa72153a46cd7da741baaba96b97432
Ildeuz1 <148572986+Ildeuz1@users.noreply.github.com>
2025-01-25 11:49:49 +0200

    l

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var force_function: Curve
+@export var force_funciton: Curve
 
 @onready var player = $Player
 @onready var sword = $BubbleSword
@@ -29,8 +29,8 @@ func _process(delta: float) -> void:
 		#sword.apply_torque_impulse(50 * sign(sword_data[2]))
 	if sign(sword_data[2]) != sign(sword.angular_velocity):
 		sword.angular_velocity = 0
-		
-	sword.apply_torque_impulse(-1500 * force_function.sample(abs(sword_data[2]) / 180)  * sign(sword_data[2]))
+	print(sword_data)
+	sword.apply_torque_impulse(-1500 * force_funciton.sample(abs(sword_data[2]) / 180)  * sign(sword_data[2]))
 	
 	$StaticBody2D.global_position = get_global_mouse_position()
 	
