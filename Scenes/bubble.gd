717ab1d873b9031d4de51503a9a36628b7fa50a5
iroguin <143116756+Iroguin@users.noreply.github.com>
2025-01-25 15:25:50 +0200

    enemies take damage

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 @onready var player = $Player
 @onready var sword = get_node("Bubble_Sword")
-@onready var bubblebody = $Bubblebody
+@onready var bubblebody = $SoftBody2D
 
 const radius = 15
 
