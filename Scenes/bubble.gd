6b81d515358f2d67b89cfab2c101ff90069e2c30
Charku4 <143108894+Charku4@users.noreply.github.com>
2025-01-26 13:27:01 +0200

    music and death

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -103,4 +103,4 @@ func _on_player_death():
 func _on_hitbox_component_area_entered(area: Area2D) -> void:
 	if area.get_child(1) is CollisionPolygon2D:
 		print("d")
-		$Health_Component_Player.health = -1000
+		$Health_Component_Player.die_player()
