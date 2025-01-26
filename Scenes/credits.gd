fd6ef70ded28ca1a00ce4f7541748f61a2715fa5
Charku4 <143108894+Charku4@users.noreply.github.com>
2025-01-26 15:25:13 +0200

    g

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -11,13 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/start_animation.tscn")
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_button_button_down() -> void:
-	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
